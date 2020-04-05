e028ae2fc446b4bb8f66e8a6e481770feb5e5d68
Vanessa Mauna <vmauna@aol.com>
2020-04-05 10:28:55 -0400

    Style: Updated Login CSS

--- help-hospitality/src/auth/loginPage/loginPage.js
+++ help-hospitality/src/auth/loginPage/loginPage.js
@@ -1,111 +1,108 @@
 import React, { Component } from 'react';
 import { Auth } from 'aws-amplify';
-import './loginPage.css'
-import People from '../../images/people.jpg'
+import './loginPage.css';
+import People from '../../images/people.jpg';
 
 class LogIn extends Component {
   state = {
-    username: "",
-    password: "",
+    username: '',
+    password: '',
     errors: {
       cognito: null,
-      blankfield: false
-    }
+      blankfield: false,
+    },
   };
 
   clearErrorState = () => {
     this.setState({
       errors: {
         cognito: null,
-        blankfield: false
-      }
+        blankfield: false,
+      },
     });
   };
 
-  handleSubmit = async event => {
+  handleSubmit = async (event) => {
     event.preventDefault();
 
     // Form validation
     this.clearErrorState();
     event.preventDefault();
-		// Form validation
-		this.clearErrorState();
-		const { password, username, } = this.state;
-		try {
-			const user = await Auth.signIn({
-				username,
-				password,
-				});
-			this.props.history.push('/restaurants');
-      console.log('User',user);
+    // Form validation
+    this.clearErrorState();
+    const { password, username } = this.state;
+    try {
+      const user = await Auth.signIn({
+        username,
+        password,
+      });
+      this.props.history.push('/restaurants');
+      console.log('User', user);
       this.props.auth.setAuthStatus(true);
-      this.props.auth.setUser(user)
-		} catch (error) {
-			let err = null;
-			!error.message ? (err = { message: error }) : (err = error);
-			this.setState({
-				errors: {
-					...this.state.errors,
-					cognito: error
-				}
-			});
-			console.log('err', err);
-		}
-	};
+      this.props.auth.setUser(user);
+    } catch (error) {
+      let err = null;
+      !error.message ? (err = { message: error }) : (err = error);
+      this.setState({
+        errors: {
+          ...this.state.errors,
+          cognito: error,
+        },
+      });
+      console.log('err', err);
+    }
+  };
 
-  onInputChange = event => {
+  handleChange = (event) => {
+    event.preventDefault();
     this.setState({
-      [event.target.id]: event.target.value
+      [event.target.name]: event.target.value,
     });
-    document.getElementById(event.target.id).classList.remove("is-danger");
   };
 
   render() {
     return (
-      <div>
-        <div id="login">
-          <img id="loginImg" src={People} alt="People Gathered"/>
-          <p id="header">Log in</p>
-          <form id="loginImg" onSubmit={this.handleSubmit}>
-          <p>
-                Username: <br/>
-                <input 
-                  className="input" 
-                  type="text"
-                  name="username"
-                  aria-describedby="usernameHelp"
-                  placeholder="Enter username or email"
-                  value={this.state.username}
-                  onChange={this.onInputChange}
-                />
-              </p>
-              <p className="control has-icons-left">
-                Password: <br/>
-                <input 
-                  className="input" 
-                  type="password"
-                  name="password"
-                  placeholder="Password"
-                  value={this.state.password}
-                  onChange={this.onInputChange}
-                />
-              </p>
-              <p>
-              <a id="link" href="/forgotpassword">Forgot password?</a>
-              </p>
-            <div className="field">
-              <p className="control">
-                <button className="button is-success">
-                  Login
-                </button>
-                <br/>
+      <div className="login">
+        <div className="loginForm">
+          <img id="loginImg" src={People} alt="table" />
+          <p id="loginFormText">
+            <br />
+            <form onChange={this.handleChange} onSubmit={this.handleSubmit}>
+              <h3>Log In To Your Account</h3>
+              <label htmlFor="username">username</label>
+              <br />
+              <input
+                id="loginInputBox"
+                value={this.state.username}
+                onChange={this.handleChange}
+                name="username"
+                placeholder="Enter your username or email"
+                type="text"
+              />
+              <br />
+              <label value={this.state.password} htmlFor="password">
+                password
+                <br />
+              </label>
+              <input
+                id="loginInputBox"
+                name="password"
+                onChange={this.handleChange}
+                type="password"
+                placeholder="Enter your password"
+              />
+              <br />
+              <button type="submit" onClick={this.handleSubmit}>
+                Login
+              </button>
+              <br />
+              <span>
                 Sign in to your account to view and edit your profile.
-                </p>
-                </div>
-          </form>
-          </div>
-          </div>
-
+              </span>
+            </form>
+          </p>
+        </div>
+      </div>
     );
   }
 }
