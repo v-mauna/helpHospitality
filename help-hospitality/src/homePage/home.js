a80cb3322c078657351ea9cd4b6bcdfd692c82ae
Vanessa Mauna <vmauna@aol.com>
2020-04-07 18:11:53 -0400

    Changed Intro & Text-Align on Home Page.

--- help-hospitality/src/homePage/home.js
+++ help-hospitality/src/homePage/home.js
@@ -2,20 +2,35 @@ import React from 'react'
 import './home.css'
 import Skyline from '../images/skyline.jpg'
 
-
-export default class Home extends React.Component{
-    render() {
-        return (
-          <main className="homePage">
-              
-              <div id='skyline'>
-                  <img src={Skyline} alt="Skyline"/>
-                  <p className='homeText'>New York's hospitality industry has been severely devastated by the COVID19 pandemic and needs our help.<br/>
-            If you're looking to donate, please browse the list of <a href="/restaurants">restaurants</a> and visit their GoFundMe page.<br/>
-            If you want to add your restaurant, we invite you to <a href='/signup'>sign up</a> and add your information.   </p>
-            </div>
-          </main>
-        )
-      }
-    }
-    
+export default class Home extends React.Component {
+  render () {
+    return (
+      <main className='homePage'>
+        <div id='skyline'>
+          <img src={Skyline} alt='Skyline' />
+          <p className='homeText'>
+            New York's restaurant industry has been severely devastated by the
+            COVID19 pandemic as has its staff members.
+            <br />
+            Our restaurants have helped us when we needed a break from our
+            kitchens, when we needed a place to crash after a long day or a
+            friendly face to talk to. <br/>
+            Now it's our turn to help them.
+            <br />
+            If you're looking to donate, please browse our list of{' '}
+            <a href='/restaurants'>restaurants</a> to find a link to their
+            donation's page. <br />
+            If you want to add your restaurant, we invite you to{' '}
+            <a href='/signup'>sign up</a> and submit your restaurant's information.
+            <br />
+            If you're a restaurant worker looking for some help, please check out our list of{' '}
+            <a href='/resources'>resources</a>
+            <br/>
+            New York and its people are strong and we will bounce back, united and stronger than before.
+            </p>
+        </div>
+      </main>
+    )
+  }
+}
+  
